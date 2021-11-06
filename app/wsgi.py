b0d25b75767e808c744c78739af068956d4379e3
peterwade153 <peterwade153@gmail.com>
2021-11-06 19:45:57 +0300

    deploy to heroku

--- app/wsgi.py
+++ app/wsgi.py
@@ -13,9 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 from utils.read_envs import read_env_file
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-if 'ENV_VAR_FILE' in os.environ:
-    file_name = os.getenv('ENV_VAR_FILE')
-    read_env_file(file_name)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.prod')
 
 application = get_wsgi_application()
